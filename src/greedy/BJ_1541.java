a96039472fd37fde1d89160ddcc94b507516a2d3
sayhara <impact82@naver.com>
2021-10-20 16:50:27 +0900

    BJ : 1931, 1026, 1541

---
+++ src/greedy/BJ_1541.java
@@ -0,0 +1,41 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+괄호를 이용해서 최소로 만들기 위해서는 결국 분할을 통해서 접근해야만 했다.
+이용할 수 있는 부호는 +와 -이므로 이를 어떤식으로 분할해야 할까 고민하다가
+마이너스(-)로 먼저 분할하고 그 분할한 덩어리들을 더하기로 분할하여 합을 구한다.
+그리고 값의 최소를 계산하기 위해 처음의 값에서 나머지 덩어리들의 값을 빼주어서 구하였다.
+ */
+
+public class BJ_1541 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int first = 0, sum = 0;
+
+        String str[] = s.split("-");
+
+        for (int i = 0; i < str.length; i++) {
+            String arr[] = str[i].split("\\+");
+            if (i == 0) {
+                for (int j = 0; j < arr.length; j++) {
+                    first += Integer.parseInt(arr[j]);
+                }
+            } else {
+                for (int j = 0; j < arr.length; j++) {
+                    sum += Integer.parseInt(arr[j]);
+                }
+            }
+        }
+
+        System.out.println(first - sum);
+
+    }
+}
---
+++ src/greedy/BJ_1931.java
@@ -0,0 +1,68 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+/*
+한번에 풀기 어려운 문제였다.
+이차 이상의 for문을 사용하면 시간초과를 일으키기에 일차 식의 for문을 이용해야 했고,
+그러기 위해서는 정렬을 이용해야 했다.
+시작시간과 관계없이 종료시간을 기준으로 정렬하면 다음번의 회의 시작시간을 짧게 할 수 있고
+그만큼 회의를 많이 진행할 수 있다. 만약 종료시간이 같은 경우라면 시작시간이 짧을수록 좋다.
+이러한 아이디어를 위해서 2개의 매개변수 비교를 위한 Comparator를 사용하였다.
+ */
+
+public class BJ_1931 {
+
+    static int N;
+    static int arr[][];
+    static boolean visited[][];
+    static int max = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][2];
+        visited = new boolean[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] > o2[1]) { // 종료시간을 기준으로 오름차순
+                    return 1;
+                } else if (o1[1] == o2[1]) { // 종료시간이 같을 경우
+                    if (o1[0] > o2[0]) {
+                        return 1; // 시작 시간을 기준으로 오름차순
+                    }
+                }
+                return -1;
+            }
+        });
+
+        int first = arr[0][1];
+        int count = 1;
+
+        for (int i = 1; i < N; i++) {
+            if (arr[i][0] >= first) {
+                first = arr[i][1];
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+}
