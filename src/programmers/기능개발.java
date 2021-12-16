333ae57eb34795123a5b76abfa02d8407fb2f82b
sayhara <impact82@naver.com>
2021-12-16 22:02:12 +0900

    review[Programmers] : 기능개발

---
+++ src/programmers/기능개발.java
@@ -0,0 +1,47 @@
+package programmers;
+
+import java.util.*;
+
+//https://void2017.tistory.com/273
+
+public class 기능개발 {
+
+    public static void main(String[] args) {
+
+        Solution solution=new Solution();
+        int[] progresses={93,30,55};
+        int[] speeds={1,30,5};
+        System.out.println(Arrays.toString(solution.solution(progresses,speeds)));
+
+    }
+
+    static class Solution {
+        public int[] solution(int[] progresses, int[] speeds) {
+            Queue<Integer> q=new LinkedList<>();
+            for(int i=0;i<progresses.length;i++){
+                q.add((int)Math.ceil((100-progresses[i])/speeds[i]));
+            }
+            ArrayList<Integer> list=new ArrayList<>();
+
+            int prev=q.poll();
+            int count=1;
+
+            while (!q.isEmpty()){
+                int cur=q.poll();
+                if(prev>=cur){
+                    count++;
+                } else{
+                    list.add(count);
+                    count=1;
+                    prev=cur;
+                }
+            }
+            list.add(count);
+            int answer[]=new int[list.size()];
+            for(int i=0;i< list.size();i++){
+                answer[i]=list.get(i);
+            }
+            return answer;
+        }
+    }
+}
