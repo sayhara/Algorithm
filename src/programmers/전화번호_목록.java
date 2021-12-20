8799d0c4fd5c011cd890bb7d2969c3d8ff8851e5
sayhara <impact82@naver.com>
2021-12-20 16:55:15 +0900

    Programmers : 전화번호 목록, 위장

--- src/programmers/전화번호_목록.java
+++ src/programmers/전화번호_목록.java
@@ -1,40 +1,35 @@
 package programmers;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.*;
+
+/*
+기존의 이중 for문을 통한 equals의 문자열 비교시에는 시간초과가
+발생하였다. 핵심은 containsKey라는 함수를 사용하는 것이었는데
+이는 해당 키값이 존재하는지의 유무를 판별하는 것이다.
+따라서, 이중 for문을 통해 문자열의 subString을 하여 값을 계산할 수 있다.
+ */
 
 public class 전화번호_목록 {
 
     public static void main(String[] args) {
         Solution solution=new Solution();
-        String phone_book[]={"123","456","789"};
+        String phone_book[]={"12","123","1235","567","88"};
         System.out.println(solution.solution(phone_book));
     }
 
     static class Solution {
         public boolean solution(String[] phone_book) {
             boolean answer = true;
-            Arrays.sort(phone_book, new Comparator<String>() { // 문자열 길이 오름차순
-                @Override
-                public int compare(String s1, String s2) {
-                    return s1.length()-s2.length();
-                }
-            });
 
             HashMap<String, Integer> map=new HashMap<>();
             for(int i=0;i<phone_book.length;i++){
                 map.put(phone_book[i],i);
             }
 
-            for(int i=0;i<phone_book.length-1;i++){
-                for(int j=i+1;j<phone_book.length;j++){
-                    String sub=phone_book[j].substring(0, phone_book[i].length());
+            for(int i=0;i<phone_book.length;i++){
+                for(int j=1;j<phone_book[i].length();j++){
+                    String sub=phone_book[i].substring(0,j);
                     if(map.containsKey(sub)){
-                        answer=false;
-                        break;
-                    }
-                    if(phone_book[i].equals(sub)){
                         answer=false;
                         break;
                     }
