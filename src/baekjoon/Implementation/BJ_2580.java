b5e097966aad70870bae22f5ec84aea57c70f0a9
sayhara <impact82@naver.com>
2022-05-02 21:18:46 +0900

    BJ : 2580

--- src/baekjoon/Implementation/BJ_2580.java
+++ src/baekjoon/Implementation/BJ_2580.java
@@ -5,83 +5,82 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+import java.util.*;
+import java.io.*;
+
 public class BJ_2580 {
-    
-    static int arr[][];
+
+    static int arr[][]=new int[9][9];
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        arr=new int[9][9];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         for(int i=0;i<9;i++){
-            StringTokenizer st=new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for(int j=0;j<9;j++){
                 arr[i][j]=Integer.parseInt(st.nextToken());
             }
         }
 
-        sodoku(0,0);
+        sudoku(0,0);
 
     }
 
-    public static void sodoku(int r, int c){ // 2차원에 대한 백트래킹 과정은 열 먼저 증가 후 -> 행 증가
+    public static void sudoku(int row, int col){
 
-        if(c==9){
-            sodoku(r+1,0);
+        if(col==9){
+            sudoku(row+1,0);
             return;
         }
 
-        if(r==9){
+        if(row==9){
             for(int i=0;i<9;i++){
                 for(int j=0;j<9;j++){
                     System.out.print(arr[i][j]+" ");
                 }
                 System.out.println();
             }
-            return;
+            System.exit(0);
         }
 
-        if(arr[r][c]==0){
+        if(arr[row][col]==0){
             for(int i=1;i<=9;i++){
-                if(check(r,c,i)){
-                    arr[r][c]=i;
-                    sodoku(r,c+1);
+                if(check(row,col,i)){
+                    arr[row][col]=i;
+                    sudoku(row,col+1);
                 }
             }
-            arr[r][c]=0;
+            arr[row][col]=0;
             return;
         }
-
-        sodoku(r,c+1);
-
+        sudoku(row,col+1);
     }
-    
-    public static boolean check(int r, int c, int value){
+
+    public static boolean check(int row, int col, int num){
 
         for(int i=0;i<9;i++){
-            if(arr[r][i]==value){
+            if(arr[row][i]==num){
                 return false;
             }
         }
 
         for(int i=0;i<9;i++){
-            if(arr[i][c]==value){
+            if(arr[i][col]==num){
                 return false;
             }
         }
 
-        int nr=r/3*3;
-        int nc=c/3*3;
+        int nr=row/3*3;
+        int nc=col/3*3;
 
         for(int i=nr;i<nr+3;i++){
             for(int j=nc;j<nc+3;j++){
-                if(arr[nr][nc]==value){
+                if(arr[i][j]==num){
                     return false;
                 }
             }
         }
         return true;
     }
-
 }
